fa4f6fc03373951b46d279730eaa596e80e7b518
Isis Meira <167538407+isismeira@users.noreply.github.com>
2024-12-23 10:07:10 -0300

    Update

--- teste3.py
+++ teste3.py
@@ -7,7 +7,6 @@ def vertical(i, j, palavra, jogo):
     if palavra_vertical == palavra:
         print(f"O jogo pode ser fechado com uma linha vertical, começando pela linha {i+1} e coluna {j+1}")
         venceu = True
-        print(venceu)
     else:
         horizontal(i, j, palavra, jogo)     
 
@@ -18,9 +17,16 @@ def horizontal(i, j, palavra, jogo):
      if palavra_horizontal == palavra:
         print(f"O jogo pode ser fechado com uma linha horizontal, começando pela linha {i+1} e coluna {j+1}")
         venceu = True
-        print(venceu)
      else:
-          print("O jogo não pode ser fechado nem pela horizontal nem pela vertical")
+          diagonal_d(i, j, palavra, jogo)
+
+# obs. lógica para a diagonal
+def diagonal_d(i, j, palavra, jogo):
+    palavra_diagonal_d = []
+    for k in range(len(palavra)):
+        palavra_diagonal_d.append(jogo[i + k][j + k])
+    if palavra_diagonal_d == palavra:
+        print(f"O jogo pode ser fechado pela diagonal, começando pela linha {i+1} e coluna {j+1}")         
 
 def verificar_jogo(jogo, palavra):
     for i in range(len(jogo)):
@@ -36,12 +42,18 @@ def verificar_jogo(jogo, palavra):
                         if venceu == True:
                              break 
                     except IndexError:
-                           print("Não é possível fechar o jogo nem com uma linha horizantal ou vertical")          
+                           try:
+                                diagonal_d(i, j, palavra, jogo)
+                                if venceu == True:
+                                     break
+                           except IndexError:
+                                print("Não foi possível solucionar o jogo")
 
-jogo = [["a","b","x","d"],
-        ["v","f","x","m"],
-        ["c","a","s","a"],
-        ["b","v","c","l"]]
+
+jogo = [["c","s","x","d"],
+        ["v","a","x","m"],
+        ["x","s","s","s"],
+        ["b","a","b","a"]]
 
 palavra = ["c","a","s","a"]
 
