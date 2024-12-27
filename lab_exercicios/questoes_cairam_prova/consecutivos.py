ac5fe6efbb106b7c5061ee66c4e1f789586bb702
Isis Meira <167538407+isismeira@users.noreply.github.com>
2024-12-27 15:04:01 -0300

    Update

--- lab_exercicios/questoes_cairam_prova/consecutivos.py
+++ lab_exercicios/questoes_cairam_prova/consecutivos.py
@@ -1,2 +1,24 @@
-
 # 2415
+# FÁCIL
+
+valores = int(input())
+lista = list(map(int, input().split()))
+
+numero_atual = lista[0]
+sequencias = []
+
+sequencia = 1
+
+for i in range(1, len(lista)):
+    if lista[i] == numero_atual:
+        sequencia += 1
+        numero_atual = lista[i]
+    else:
+        numero_atual = lista[i]
+        sequencias.append(sequencia)
+        sequencia = 1    
+
+# fazer o append da última sequência
+sequencias.append(sequencia)
+
+print(max(sequencias))
--- lab_exercicios/questoes_cairam_prova/jogo_da_estrategia.py
+++ lab_exercicios/questoes_cairam_prova/jogo_da_estrategia.py
@@ -1,1 +1,25 @@
 # 1940
+# MÉDIO (mas fiz acertei de segunda)
+
+values = input().split()
+qtdd_jogadores = int(values[0])
+qtdd_rodadas = int(values[1])
+
+lista = list(map(int, input().split()))
+total_pontos = []
+
+for i in range(qtdd_jogadores):
+    soma = 0
+    for j in range(i, len(lista), qtdd_jogadores):
+        soma += lista[j]
+    total_pontos.append(soma)    
+
+maior_valor = 0
+atual_campeao = 0
+
+for i in range(len(total_pontos) - 1, -1, -1):
+    if total_pontos[i] > maior_valor:
+        atual_campeao = i + 1
+        maior_valor = total_pontos[i]
+
+print(atual_campeao)
